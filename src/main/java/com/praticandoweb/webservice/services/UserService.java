60bd66c2ad6c1d3b4755abe660e85a7baae8f9c6
AlexbarrosDev <alexbarros.dev@gmail.com>
2025-10-06 18:55:18 -0300

    Service layer, component registration

---
+++ src/main/java/com/praticandoweb/webservice/services/UserService.java
@@ -0,0 +1,25 @@
+package com.praticandoweb.webservice.services;
+
+import com.praticandoweb.webservice.entities.User;
+import com.praticandoweb.webservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
