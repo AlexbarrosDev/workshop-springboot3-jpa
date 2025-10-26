eed8b4b860434d0abbc5bf4514ad9ac60e471eb7
AlexbarrosDev <alexbarros.dev@gmail.com>
2025-10-26 18:52:50 -0300

    Exception handling - delete

--- src/main/java/com/praticandoweb/webservice/services/UserService.java
+++ src/main/java/com/praticandoweb/webservice/services/UserService.java
@@ -2,8 +2,11 @@ package com.praticandoweb.webservice.services;
 
 import com.praticandoweb.webservice.entities.User;
 import com.praticandoweb.webservice.repositories.UserRepository;
+import com.praticandoweb.webservice.services.exceptions.DatabaseException;
 import com.praticandoweb.webservice.services.exceptions.ResourcesNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourcesNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
